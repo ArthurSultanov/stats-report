1c621a27dbb382b38d03e9c8a369ea2f4ca9b171
lu4ik-dev <dimasmi068@gmail.com>
2023-11-07 21:38:15 +0300

    error with login

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min';
+
+function App() {
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
